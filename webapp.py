5d5b76e16c289dbae0dc31b68ca64c41525042e9
Benjamin Pearce <hawk.git@bcpearce.com>
2016-08-04 00:37:38 -0400

    created simple web app

---
+++ webapp.py
@@ -0,0 +1,32 @@
+import sys
+from flask import Flask, render_template, request
+from ble_led import BleLed
+
+app = Flask(__name__)
+
+with open('ble_dev') as f:
+    dev = f.read().strip()
+light = BleLed(dev)
+
+@app.route('/')
+def root():
+    return render_template('index.html', color="#FFFFFF")
+
+@app.route('/solid_color', methods=['POST'])
+def solid_color():
+    color = request.form['color'][1:]
+    rgb = [int(x, 16) for x in [color[i:i+2] for i in range(0, len(color), 2)]]
+    light.set_solid_color(rgb)
+    return render_template('index.html', color="#{0}".format(color))
+
+@app.route('/turn_off', methods=['GET', 'POST'])
+def turn_off():
+    light.turn_off()
+    return render_template('index.html', color="#FFFFFF")
+
+
+if __name__ == "__main__":
+    ip = '0.0.0.0'
+    if len(sys.argv) == 2:
+        ip = sys.argv[1]
+    app.run(host = ip)
