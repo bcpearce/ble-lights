49a532cb3a0902c9006b2a39298d3a902ed719fb
Benjamin Pearce <hawk.git@bcpearce.com>
2016-10-02 00:02:37 -0400

    added rotation colors

--- ble_led.py
+++ ble_led.py
@@ -6,9 +6,30 @@ from bluepy.btle import Peripheral
 # 0x0043 register
 # [0x56, red, green, blue, ???, 0xf0 for on or 0x0f for off, 0xaa]
 
+
+
 class BleLed(Peripheral):
 
     SOLID_COLOR_HDL = 0x0043
+
+    SIX_COLORS = {
+        'red': (0xff, 0x00, 0x00),
+        'yellow': (0xff, 0xff, 0x00),
+        'green': (0x00, 0xff, 0x00),
+        'cyan': (0x00, 0xff, 0xff),
+        'blue': (0x00, 0x00, 0xff),
+        'magenta': (0xff, 0x00, 0xff)
+    }
+
+    SEVEN_COLORS = {
+        'red': (0xff, 0x00, 0x00),
+        'yellow': (0xff, 0xff, 0x00),
+        'green': (0x00, 0xff, 0x00),
+        'cyan': (0x00, 0xff, 0xff),
+        'blue': (0x00, 0x00, 0xff),
+        'magenta': (0xff, 0x00, 0xff),
+        'white': (0xff, 0xff, 0xff)
+    }
 
     def norm_color(self, color):
         red, green, blue = color
@@ -66,13 +87,28 @@ class BleLed(Peripheral):
             i += 1
 
     def fade_to(self, color=(0xff, 0xff, 0xff), duty=0.1):
-        diff_set = [1 if (y-x) > 0 else -1 for x, y in zip(self.my_color, color)]
+        diff_set = [1 if (y-x) > 0 else 0 for y, x in zip(color, self.my_color)]
+        diff_set = [-1 if (y-x) < 0 else z for y, x, z in zip(color, self.my_color, diff_set)]
         while not all(x == y for x, y in zip(self.my_color, color)):
             self.my_color = [x+y for x, y in zip(self.my_color, diff_set)]
             self.my_color = [0x01 if x < 0 else x for x in self.my_color]
             self.my_color = [0xff if x > 0xff else x for x in self.my_color]
             self.set_solid_color(self.my_color)
             time.sleep(duty)
+
+    def multi_fade(self, color_set, duty=0.1):
+        self.set_solid_color(color_set[-1])
+        while True:
+            for color in color_set:
+                self.fade_to(color, duty)
+
+    def six_fade(self, duty=0.1):
+        color_wheel = self.SIX_COLORS.values()
+        self.multi_fade(color_wheel, duty)
+
+    def seven_fade(self, duty=0.1):
+        color_wheel = self.SEVEN_COLORS.values()
+        self.multi_fade(color_wheel, duty)
 
 
 if __name__ == "__main__":
@@ -83,8 +119,7 @@ if __name__ == "__main__":
         light = BleLed(ble_dev)
 
         light.blink(times=3)
-        light.set_solid_color((0x01, 0x01, 0x01))
-        light.fade_to((0xff, 0xff, 0xff))
+        light.seven_fade(duty=0.05)
 
     finally:
         light.turn_off()
