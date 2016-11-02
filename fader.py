2a43d639479135645a678e5700dbc40f1dad8db5
Benjamin Pearce <hawk.git@bcpearce.com>
2016-11-01 22:18:32 -0400

    added fades to webapp

--- fader.py
+++ fader.py
@@ -2,23 +2,35 @@
 
 import ble_led, threading, subprocess, inspect, time
 
-class Fader(threading.Thread):
+class Fader():
 
     def __init__(self, ble_light):
         self.ble_light = ble_light
         self.method = ble_light.seven_fade
         self.args = inspect.getargspec(self.method)[3]
         self.flag = threading.Event()
-        super(self.__class__, self).__init__()
+        self.thread = threading.Thread(target=self.run)
+        #super(self.__class__, self).__init__()
+
+    def set_solid_color(self, rgb):
+        self.end_seq()
+        self.ble_light.set_solid_color(rgb)
+
+    def turn_off(self):
+        self.end_seq()
+        self.ble_light.turn_off()
 
     def set_duty(self, duty):
         self.ble_light.duby = duty
 
     def set_six_fade(self):
-        self.method = ble_light.six_fade
+        self.method = self.ble_light.six_fade
 
     def set_seven_fade(self):
-        self.method = ble_light.seven_fade
+        self.method = self.ble_light.seven_fade
+
+    def start(self):
+        self.thread.start()
 
     def run(self):
         self.flag.clear()
@@ -26,6 +38,7 @@ class Fader(threading.Thread):
 
     def end_seq(self):
         self.flag.set()
+        self.thread = threading.Thread(target=self.run)
 
     def __enter__(self):
         return self
--- webapp.py
+++ webapp.py
@@ -1,12 +1,14 @@
 import sys
 from flask import Flask, render_template, request
 from ble_led import BleLed
+from fader import Fader
 
 app = Flask(__name__)
 
 with open('ble_dev') as f:
     dev = f.read().strip()
 light = BleLed(0.05, dev)
+fader = Fader(light)
 
 @app.route('/')
 def root():
@@ -16,14 +18,28 @@ def root():
 def solid_color():
     color = request.form['color'][1:]
     rgb = [int(x, 16) for x in [color[i:i+2] for i in range(0, len(color), 2)]]
-    light.set_solid_color(rgb)
+    fader.set_solid_color(rgb)
     return render_template('index.html', color="#{0}".format(color))
 
 @app.route('/turn_off', methods=['GET', 'POST'])
 def turn_off():
-    light.turn_off()
+    fader.end_seq()
+    fader.turn_off()
     return render_template('index.html', color="#FFFFFF")
 
+@app.route('/six_fade', methods=['POST'])
+def six_fade():
+    fader.end_seq()
+    fader.set_six_fade()
+    fader.start()
+    return render_template('index.html', color="#FFFFFF")
+
+@app.route('/seven_fade', methods=['POST'])
+def seven_fade():
+    fader.end_seq()
+    fader.set_seven_fade()
+    fader.start()
+    return render_template('index.html', color="#FFFFFF")
 
 if __name__ == "__main__":
     ip = '0.0.0.0'
